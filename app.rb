5f0d388a938e6df425b6480606178fcd685a1d32
sdyer777 <sdyer777@yahoo.com>
2019-06-25 18:31:35 -0400

    returning array of pets

--- app.rb
+++ app.rb
@@ -3,11 +3,35 @@ require 'sinatra'
 require "sinatra/json"
 require 'sinatra/activerecord'
 
-
+# GET /
+# Return all pets
 get '/' do
+  # return Pet.all
   # return json {'text': 'Awesome!'}
-  json text: 'Awesome!'
+  # json text: 'Awesome!'
+  content_type :json
+  # { :key1 => 'value1', :key2 => 'value2' }.to_json
+  { 
+    "pets": [
+                 { 
+                    "id":           1,
+                    "name":         "Patty",  
+                    "location":     "Quincy"
+                 },
+                 { 
+                    "id":           2,
+                    "name":         "Max",  
+                    "location":     "Chicago"
+                 },
+                 { 
+                    "id":           3,
+                    "name":         "Fifi",  
+                    "location":     "Paris"
+                 }
+    ] 
+}.to_json
 end
+
 
 get '/jonyive' do
   # return json {'text': 'Aluminium'}
