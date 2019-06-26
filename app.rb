b5f85b302b4307ececc50bbb1fc479023638801e
sdyer777 <sdyer777@yahoo.com>
2019-06-26 19:09:56 -0400

    code cleanup

--- app.rb
+++ app.rb
@@ -14,25 +14,12 @@ get '/' do
   res.to_json
 end
 
+# GET /:id
 # Return specified pet
 get "/:id" do
   conn = PG::Connection.open(ENV['DATABASE_URL'])
   res  = conn.exec('SELECT * from pets where id = ' + params[:id])
   conn.close
   res.to_json
-
-#response = 'ID=[' + params[:id] + ']'
-=begin
-  content_type :json
-  { 
-    "id":           1,
-    "name":         "PattyONE",
-    "type":         "DogONE",
-    "breed":        "Wheaten Terrier",  
-    "location":     "Quincy",
-    "latitude":     42.2529,
-    "longitude":    71.0023
-  }.to_json
-=end
 end
 
