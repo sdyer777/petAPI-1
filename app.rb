927a15f273cba74e7602c79313350a63d14ff914
sdyer777 <sdyer777@yahoo.com>
2019-06-26 21:59:41 -0400

    more debug code - dumping headers

--- app.rb
+++ app.rb
@@ -29,6 +29,7 @@ post '/addPet' do
   puts "GOT TO ADD PET - "  +Time.new.inspect
   puts "GOT TO ADD PET - zz params next:"
   puts "GOT TO ADD PET - params=["  + params.to_s + "]"
+  puts "GOT TO ADD PET - headers=["  + headers.to_s + "]"
   query = "insert into pets (name, type, breed, location, latitude, longitude) VALUES ("
   query += "'" + params[:name] + "', "
   query += "'" + params[:type] + "', "
